42c8ba621cc8056a1721be3c640c8cf7139cf853
changdiqing <changdiqing@live.cn>
2020-05-31 16:12:33 +0200

    connection to mongodb atlas and basic CRUD

--- README.md
+++ README.md
@@ -2,9 +2,21 @@
 
 Install Packages: 
 ### `npm install`
+
 Start Server: 
 ### `npm run devstart`
-Browser: [http://localhost:8080](http://localhost:8080)  <br/>
-<br/>
+
+Browser: [http://localhost:8080](http://localhost:8080)
+
+To change database url, go to `db.js` and edit:
+### `const dbConnectionUrl;`
+
+To change database and collection name, go to `server.ja` and edit:
+
+----
+### const dbName 
+### const collectionName
+----
+
 Stop Server: 
 ### `Ctrl + C`
--- server.js
+++ server.js
@@ -1,8 +1,103 @@
-var http = require('http');
+const express = require("express");
+const server = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + Date() + "<br>");
-  res.write(req.url + "<br>");
-  res.end('END');
-}).listen(8080);
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = 8080;
+
+// db setup
+const db = require("./db");
+const dbName = "data";
+const collectionName = "shopping_requests";
+
+// db init
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+    // get all items
+    dbCollection.find().toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+    // CRUD
+    // create
+    server.post("/items", (request, response) => {
+        const item = request.body;
+        dbCollection.insertOne(item, (error, result) => {  // callback of insertOne
+            if(error) throw error;
+            // return updated collection
+            dbCollection.find().toArray((_error, _result) => {
+                if (_error) throw _error;
+                response.json(_result);
+            });
+        });
+    })
+
+    // read by id
+    server.get("/items/:id", (request,response) => {
+        const itemId = request.params.id;
+
+        dbCollection.findOne({ id: itemId}, (error, result) => {
+            if (error) throw error;
+
+            // return item
+            response.json(result);
+        })
+    })
+
+    // read all
+    server.get("/items", (request, response) => {
+        // return updated list
+        dbCollection.find().toArray((error, result) => {
+            if (error) throw error;
+            response.json(result);
+        });
+    });
+
+    // update by id
+    server.put("/items/:id", (request, response) => {
+        const itemId = request.params.id;
+        const item = request.body;
+        console.log("Update item: ", itemId, " to be ", item);
+
+        dbCollection.updateOne({ id: itemId}, {$set: item}, (error, result) => {
+            if(error) throw error;
+            // // send back updated collection
+            dbCollection.find().toArray((_error, _result) => {
+                if(_error) throw _error;
+                response.json(_result);
+            });
+        });
+    })
+
+    // delete by id
+    server.delete("/items/:id", (request, response) => {
+        const itemId = request.params.id;
+        console.log("Delete item with id: ", itemId);
+
+        dbCollection.deleteOne({ id: itemId}, (error, result) => {
+            if (error) throw error;
+            // send back updated collection
+            dbCollection.find().toArray((_error, _result) => {
+                if (_error) throw _error;
+                response.json(_result);
+            });
+        });
+    });
+
+
+}, function(err) { // failureCallback
+    throw (err);
+});
+
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
+
+
+
+
+
+// 
