bebd4864346fdd14c2691def9be78ee414c2cba9
Jonathan <jjjooonnn@gmail.com>
2020-07-18 12:42:26 +0200

    restored list customer request
    deleted by accident earlier

--- src/routes/requestRoutes.js
+++ src/routes/requestRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const middlewares = require ('../middlewares');
 const customerRequestController = require('../controllers/requestController');
 
+router.get('/', customerRequestController.list);
 router.post('/', middlewares.checkAuthentication, customerRequestController.create);
 router.get('/:id', customerRequestController.read);
 router.put('/:id', middlewares.checkAuthentication, customerRequestController.update);
