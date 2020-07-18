3b5b85798dfe7139509802fc732d8fdeb7cf25b9
changdiqing <changdiqing@live.cn>
2020-07-18 19:36:02 +0200

    removed commented routes

--- src/routes/shopsApi.js
+++ src/routes/shopsApi.js
@@ -7,18 +7,15 @@ const shopController = require('../controllers/shop');
 
 // List all shops with details
 router.get('/', shopController.list);
-// router.post('/', middlewares.checkAuthentication, shopController.create())
 
 // create a new shop
 router.post('/', shopController.create);
 
 // get shop data by ObjectId
 router.get('/:id', shopController.read);
-// router.put('/:id', middlewares.checkAuthentication, shopController.update);
 
 // update shop with ObjectId
 router.put('/:id', shopController.update);
-// router.delete('/:id', middlwares.checkAuthentication, shopController.remove);
 
 // delete shop with ObjectId
 router.delete('/:id',  shopController.remove);
